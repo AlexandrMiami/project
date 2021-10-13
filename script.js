9e5e44d3df9edb13f6c3e171ac9b59c6313dfafb
Alexandr <crim.egorov@yandex.ru>
2021-10-13 20:38:29 +0300

    Измеил css

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let number = 4.6;
+
+
+
+const obj = {
+    name: 'John',
+    age: 25,
+    penis: '24 sm'
+};
+
+console.log(obj.penis);
+
+const massive = ['BMW', 'AUDI', 'MERS']
+console.log(massive[1]);
+
+
+
+const vopros = [];
+vopros[0] = prompt('Какая у вас машина?');
+vopros[1] = prompt('Довольны своей машиной?');
+vopros[2] = prompt('Сколько расход?');
+
+document.write(vopros);
